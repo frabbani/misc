b3d5ab027fdd467cca943f61e9eee8dc409eb90a
frabbani <f.rabbani@gmail.com>
2024-06-01 14:48:53 -0500

    fix typechecking

--- js/mygl/mygl.js
+++ js/mygl/mygl.js
@@ -2,21 +2,27 @@ var mygl = mygl || {};
 
 
 function is_scalar(s) {
-  return typeof s === 'number' && !Number.isInteger(s);
+  return typeof s === 'number'; // && !Number.isInteger(s);
 }
 
-function is_vec4(v) {
-  if (Array.isArray(v) && v.length == 4)
+function is_vec4(a) {
+  if (Array.isArray(a) && a.length == 4) {
+    // console.log("    - " + typeof (a[0]) + ' (' + Number.isInteger(a[0]) + ')');
+    // console.log("    - " + typeof (a[1]) + ' (' + Number.isInteger(a[1]) + ')');
+    // console.log("    - " + typeof (a[2]) + ' (' + Number.isInteger(a[2]) + ')');
+    // console.log("    - " + typeof (a[3]) + ' (' + Number.isInteger(a[3]) + ')');
     return is_scalar(a[0]) && is_scalar(a[1]) && is_scalar(a[2]) && is_scalar(a[3]);
+  }
   return false;
 }
-
 mygl.vec4 = function () {
   return [0.0, 0.0, 0.0, 0.0];
 }
 
 mygl.vec4set = function (x, y, z, w) {
-  return [x, y, z, w];
+  if (is_scalar(x) && is_scalar(y) && is_scalar(z) && is_scalar(w))
+    return [x, y, z, w];
+  return mygl.vec4();
 }
 
 mygl.vec4dot = function (u, v) {
@@ -51,9 +57,16 @@ mygl.mat4scale = function (sx, sy, sz) {
 }
 
 function is_mat4(a) {
-  if (Array.isArray(a))
-    if (a.length == 4)
+  if (Array.isArray(a)) {
+    if (a.length == 4) {
+      // console.log("matrix format:");
+      // console.log(" * " + is_vec4(a[0]));
+      // console.log(" * " + is_vec4(a[1]));
+      // console.log(" * " + is_vec4(a[2]));
+      // console.log(" * " + is_vec4(a[3]));
       return is_vec4(a[0]) && is_vec4(a[1]) && is_vec4(a[2]) && is_vec4(a[3]);
+    }
+  }
   return false;
 }
 
@@ -78,4 +91,21 @@ mygl.mat4mul = function (a, b) {
     }
   }
   return mygl.mat4();
+}
+
+
+mygl.mat4flat = function (a) {
+  let r = [];
+  let n = 0;
+  if (is_mat4(a)) {
+    for (let i = 0; i < a.length; i++)
+      for (let j = 0; j < a[i].length; j++)
+        r[n++] = a[j][i];
+  }
+  else {
+    for (let i = 0; i < 16; i++)
+      r[n++] = 0.0;
+
+  }
+  return r;
 }
